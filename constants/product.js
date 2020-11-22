e847135df0c8dab194fe8f018674177388146f29
unknown <anton13jeejo@gmail.com>
2020-11-22 14:59:08 +0530

    Initial commit

---
+++ constants/product.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: "1",
+    title: "Men Charcoal Grey & Beige Slim Fit Checked Casual Shirt",
+    price: 1149,
+    image:
+      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10187585/2019/9/26/b6fcbfcf-ac72-406f-b909-19f07c81700c1569503106095-WROGN-Men-Charcoal-Grey--Beige-Slim-Fit-Checked-Casual-Shirt-1.jpg",
+  },
+  {
+    id: "2",
+    title: "Unisex Yellow & Black Printed Duffel Bag",
+    price: 799,
+    image:
+      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12666182/2020/10/20/50b77967-1e3d-4658-b7f4-ed464edf56c51603192574654-WROGN-Unisex-Duffel-Bag-3481603192572587-1.jpg",
+  },
+  {
+    id: "3",
+    title: "Noise Unisex Grey Colorfit Pro 2 Smartwatch wrb-sw",
+    price: 2799,
+    image:
+      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2019/10/22/b64ad159-21e3-479b-80c9-37f053d756141571696203491-1.jpg",
+  },
+  {
+    id: "4",
+    title: "Shots X5 PRO Truly Wireless Bluetooth Headset - Charcoal Grey",
+    price: 3519,
+    image:
+      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11811102/2020/5/11/43e09f84-69b5-48e5-a6e9-654777c34e0e1589180190482-NOISE-Unisex-Black-Solid-Shots-X5-PRO-Truly-Wireless-Bluetoo-1.jpg",
+  },
+  {
+    id: "5",
+    title: "Men Red Analogue Watch 0830617",
+    price: 8540,
+    image:
+      "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12496654/2020/10/8/f9df6db2-229d-4769-bd69-3af30f3592aa1602137511466-Scuderia-Ferrari-Red-Rev-Analogue-Red-Round-Diam-Mens-Watch--1.jpg",
+  },
+  {
+    id: "6",
+    title: "Apple iPhone 12 Mini 64 GB, Blue",
+    price: 69900,
+    image:
+      "https://www.reliancedigital.in/medias/Apple-12-Mini-Smartphones-491901546-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxMzg3NzF8aW1hZ2UvanBlZ3xpbWFnZXMvaDU0L2gxMS85NDA3NzY3NDEyNzY2LmpwZ3w2NDVlNTAzMzBlOWZjYTQ1ZjU3N2NkNzI4MTExNmRhOWNjMWY5ZjFkNWRmMDUyYjNiODU3Nzc0MjZkMWM0ZjZj",
+  },
+];
+
+export { products };

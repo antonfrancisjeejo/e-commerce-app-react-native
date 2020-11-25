a27a7b3bc2171b50a2a8144141439057a30d213d
unknown <anton13jeejo@gmail.com>
2020-11-25 19:17:41 +0530

    Initial commit

---
+++ screens/TermsandConditions.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const TermsandConditions = () => {
+  return (
+    <View>
+      <Text>TermsandConditions</Text>
+    </View>
+  );
+};
+
+export default TermsandConditions;
+
+const styles = StyleSheet.create({});
